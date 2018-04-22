b8898b2deec32a3f428a9465085eee84ede11bd3
MrJohn <josiah.peterson@aol.com>
2018-04-21 22:14:24 -0700

    Files can be edited now

--- js/gui.js
+++ js/gui.js
@@ -1,12 +1,13 @@
 const remote = require('electron').remote
 const dialog = remote.dialog;
 var content;
+var byte_data = new Uint8Array;
 var fs = require('fs');
-var byte_data = [];
 var bytes = [];
 var focusByte;
 var altSelect = 0;
 var currentFile = "";
+var MAX_BYTE_SIZE = 256;
 document.onclick = function(e){
 	if(!contains(e.target.classList, "menuItem")){
 		closeAllMenus();
@@ -19,8 +20,17 @@ window.onkeydown = function(e){
 	if(e.keyCode == 13){
 		if(document.getElementById("editText") === document.activeElement){
 			if(focusByte.innerText != document.getElementById("editText").value.toUpperCase()){
-				focusByte.innerText = document.getElementById("editText").value.toUpperCase();
-				focusByte.classList.add("modified");
+				var val = getDecimalFromParse(document.getElementById("editText").value);
+				console.log(val);
+				if(val < MAX_BYTE_SIZE){
+					focusByte.innerText = formatHex(val.toString(16).toUpperCase());
+					byte_data[focusByte.getAttribute("data-index")] = val;
+					focusByte.classList.add("modified");
+					status("Ready");
+				}
+				else{
+					error("Input value for byte larger than MAX BYTE SIZE(255)");
+				}
 			}
 		}
 	}
@@ -38,8 +48,12 @@ function openFile(){
 	dialog.showOpenDialog({properties: ['openFile']}, 
 	function(filename){
 		currentFile = filename[0];
-		fs.readFile(filename[0], 'ascii', function (err, data){
+		fs.readFile(filename[0], 'utf8', 'rb', function (err, data){
 			if(err) return console.log(err);
+			var container = document.getElementsByClassName("content")[0];
+			container.innerHTML = "";
+			bytes = [];
+			byte_data = [];
 			content = data;
 			for(var i = 0; i < data.length; i++){
 				var hex = content.charCodeAt(i).toString(16);
@@ -49,7 +63,7 @@ function openFile(){
 				bytes.push(hex.toUpperCase());
 				byte_data.push(content.charCodeAt(i));
 			}
-			var container = document.getElementsByClassName("content")[0];
+			console.log(byte_data);
 			for(var i = 0; i < bytes.length; i++){
 				container.innerHTML += "<div class='byteBlock' onclick='byteEvent(this)' onmouseover='updateLine(this)' data-index='" + i + "'>" + bytes[i] + "</div>";
 			}
@@ -61,10 +75,13 @@ function saveFile(){
 	dialog.showSaveDialog({properties: ['saveFile']}, 
 	function(filename){
 		var content = "";
-		for(var i = 0; i < byte_data.length; i++){
-			content += String.fromCharCode(byte_data[i]);
-		}
-		fs.writeFile(filename, content, 'ascii', function(err){
+		const byte_array = new Uint8Array(byte_data.length);
+		byte_array.set(byte_data);
+		byte_array.forEach(function(element, index, array){
+			console.log(element);
+			content += String.fromCharCode(element);
+		});
+		fs.writeFile(filename, content, 'utf8', '0o666', 'wb', function(err){
 			if(err) return console.log(err);
 		});
 	});
@@ -112,6 +129,7 @@ function byteEvent(item){
 	item.classList.toggle("active");
 	item.classList.toggle("focused");
 	focusByte = item;
+	document.getElementById("editText").disabled = false;
 }
 
 function updateLine(item){
@@ -174,4 +192,43 @@ function contains(arr, item){
 		}
 	}
 	return false;
+}
+
+function getDecimalFromParse(input){
+	if(input.split(" ").length > 1){
+		return convertBinToDec(input.split(" ")[0] + input.split(" ")[1]);
+	}
+	else if(input.match(/[0-9]+D/) != null){
+		return parseInt(input.split("D")[0]);
+	}
+	else{
+		return parseInt(input, 16);
+	}
+}
+
+function formatHex(str){
+	while(str.length < 2){
+		str = "0" + str;
+	}
+	return str;
+}
+
+function convertBinToDec(bin){
+	var result = 0;
+	for(var i = bin.length - 1; i >= 0; i--){
+		result += parseInt(bin.charAt(i))*Math.pow(2,  (bin.length - 1) - i); 
+	}
+	return result;
+}
+
+function status(val){
+	var statusLabel = document.getElementById("status");
+	statusLabel.style.color = "#FFFFFF";
+	statusLabel.innerText = val;
+}
+
+function error(val){
+	var statusLabel = document.getElementById("status");
+	statusLabel.style.color = "#FF2A2A";
+	statusLabel.innerText = val;
 }
